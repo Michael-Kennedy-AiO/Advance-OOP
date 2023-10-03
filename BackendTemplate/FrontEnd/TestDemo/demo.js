ae0559d48b4897f369a5b9bcc20aca47bc9f75f9
buiminhquang <buiminhquang2002@gmailcom>
2023-10-03 18:17:04 +0700

    demo

---
+++ BackendTemplate/FrontEnd/TestDemo/demo.js
@@ -0,0 +1,9 @@
+const horse1 = document.getElementById('horse1');
+    const horse2 = document.getElementById('horse2');
+    const horse3 = document.getElementById('horse3');
+    const horse4 = document.getElementById('horse4');
+
+    horse1.style.animationDuration = '5s';
+    horse2.style.animationDuration = '3s';
+    horse3.style.animationDuration = '2s';
+    horse4.style.animationDuration = '4s';
